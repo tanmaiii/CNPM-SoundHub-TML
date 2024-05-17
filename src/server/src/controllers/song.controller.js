1ca0e1015ddad4c7bb055217a2d30724c7ec6da9
tanmaiii <tanmai833@gmail.com>
2024-05-17 12:47:46 +0700

    CT-45 API Xay dung api yeu thich bai hat

--- src/server/src/controllers/song.controller.js
+++ src/server/src/controllers/song.controller.js
@@ -99,9 +99,74 @@ export const deleteSong = async (req, res) => {
   }
 };
 
+export const likeSong = async (req, res) => {
+  try {
+    const token = req.headers["authorization"];
+    const userInfo = await jwtService.verifyToken(token);
+
+    Song.like(req.params.songId, userInfo.id, (err, data) => {
+      if (err) {
+        const conflictError = err;
+        return res.status(401).json({ conflictError });
+      } else {
+        return res.json(data);
+      }
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const unLikeSong = async (req, res) => {
+  try {
+    const token = req.headers["authorization"];
+    const userInfo = await jwtService.verifyToken(token);
+
+    Song.unlike(req.params.songId, userInfo.id, (err, data) => {
+      if (err) {
+        const conflictError = err;
+        return res.status(401).json({ conflictError });
+      } else {
+        return res.json(data);
+      }
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const checkSongLiked = async (req, res) => {
+  try {
+    const token = req.headers["authorization"];
+    const userInfo = await jwtService.verifyToken(token);
+
+    // Tìm bài hát trong database dựa trên songId
+    Song.findById(req.params.songId, userInfo.id, (err, song) => {
+      if (err || !song) {
+        return res.status(404).json({ conflictError: "Bài hát không tồn tại" });
+      } else {
+        // Kiểm tra xem userId có tồn tại trong danh sách người thích của bài hát hay không
+        Song.findUserLike(req.params.songId, (err, data) => {
+          if (data) {
+            const isLiked = data.includes(userInfo.id);
+            return res.status(200).json({ isLiked });
+          } else {
+            return res.status(200).json({ isLiked: false });
+          }
+        });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
 export default {
   getSong,
   createSong,
   updateSong,
   deleteSong,
+  likeSong,
+  unLikeSong,
+  checkSongLiked,
 };
--- src/server/src/model/song.model.js
+++ src/server/src/model/song.model.js
@@ -61,38 +61,147 @@ Song.delete = (songId, result) => {
   );
 };
 
-
 Song.findById = (songId, userId, result) => {
+  db.query(
+    `SELECT s.*, u.name as author ` +
+      ` FROM songs as s` +
+      ` LEFT JOIN users AS u ON s.user_id = u.id` +
+      ` WHERE s.id = ? AND s.is_deleted = 0`,
+    [songId],
+    (err, song) => {
+      if (err) {
+        // Xử lý lỗi
+        console.error("Error while querying database:", err);
+        result(err, null);
+        return;
+      }
+
+      if (!song || !song.length) {
+        result("Không tìm thấy !", null);
+        return;
+      }
+
+      // Kiểm tra quyền truy cập
+      if (song[0].public === 0 && song[0].user_id !== userId) {
+        result("Bài hát đang ẩn !", null);
+        return;
+      }
+
+      // Trả về kết quả
+      result(null, song[0]);
+    }
+  );
+};
+
+Song.findUserLike = async (songId, result) => {
+  db.query(
+    `SELECT user_id FROM favourite_songs as fs WHERE fs.song_id = ?`,
+    [songId],
+    (err, data) => {
+      if (err) {
+        result(err, null);
+        return;
+      }
+
+      if (data.length) {
+        result(
+          null,
+          data.map((user) => user.user_id)
+        );
+        return;
+      }
+
+      result(null, null);
+    }
+  );
+};
+
+Song.like = (songId, userId, result) => {
+  // Tìm kiếm bài hát theo id
+  Song.findById(songId, userId, (err, song) => {
+    if (err) {
+      console.log("ERROR", err);
+      result(err, null);
+      return;
+    }
+
+    if (!song) {
+      console.log("Song not found");
+      result("Bài hát không tồn tại", null);
+      return;
+    }
+
+    // Kiểm tra xem người dùng đã thích bài hát này chưa
     db.query(
-      `SELECT s.*, u.name as author ` +
-        ` FROM songs as s` +
-        ` LEFT JOIN users AS u ON s.user_id = u.id` +
-        ` WHERE s.id = ? AND s.is_deleted = 0`,
-      [songId],
-      (err, song) => {
-        if (err) {
-          // Xử lý lỗi
-          console.error("Error while querying database:", err);
-          result(err, null);
+      "SELECT * FROM favourite_songs WHERE user_id = ? AND song_id = ?",
+      [userId, songId],
+      (queryErr, rows) => {
+        if (queryErr) {
+          console.log("ERROR", queryErr);
+          result(queryErr, null);
           return;
         }
-  
-        if (!song || !song.length) {
-          result("Không tìm thấy !", null);
+
+        // Nếu người dùng đã thích bài hát này trước đó, không thực hiện thêm
+        if (rows.length > 0) {
+          console.log("Bài hát đã được thích bởi người dùng");
+          result("Bài hát đã được thích bởi người dùng", null);
           return;
         }
-  
-        // Kiểm tra quyền truy cập
-        if (song[0].public === 0 && song[0].user_id !== userId) {
-          result("Bài hát đang ẩn !", null);
-          return;
-        }
-  
-        // Trả về kết quả
-        result(null, song[0]);
+
+        // Thêm bài hát vào danh sách bài hát yêu thích của người dùng
+        db.query(
+          "INSERT INTO favourite_songs SET `user_id` = ?, `song_id`= ?",
+          [userId, songId],
+          (insertErr, insertRes) => {
+            if (insertErr) {
+              console.log("ERROR", insertErr);
+              result(insertErr, null);
+              return;
+            }
+            // Trả về thông tin bài hát đã được thêm vào danh sách yêu thích
+            result(null, { song_id: songId, user_id: userId });
+          }
+        );
       }
     );
-  };
+  });
+};
 
+Song.unlike = (songId, userId, result) => {
+  // Kiểm tra xem bài hát đã được yêu thích bởi người dùng chưa
+  db.query(
+    "SELECT * FROM favourite_songs WHERE user_id = ? AND song_id = ?",
+    [userId, songId],
+    (queryErr, rows) => {
+      if (queryErr) {
+        console.log("ERROR", queryErr);
+        result(queryErr, null);
+        return;
+      }
 
-export default Song
+      // Nếu không tìm thấy bài hát trong danh sách yêu thích của người dùng, trả về lỗi
+      if (rows.length === 0) {
+        result("Bài hát không được thích bởi người dùng", null);
+        return;
+      }
+
+      // Xóa bài hát khỏi danh sách yêu thích của người dùng
+      db.query(
+        "DELETE FROM favourite_songs WHERE user_id = ? AND song_id = ?",
+        [userId, songId],
+        (deleteErr, deleteRes) => {
+          if (deleteErr) {
+            console.log("ERROR", deleteErr);
+            result(deleteErr, null);
+            return;
+          }
+          // Trả về thông tin bài hát đã bị xóa khỏi danh sách yêu thích
+          result(null, { song_id: songId, user_id: userId });
+        }
+      );
+    }
+  );
+};
+
+export default Song;
